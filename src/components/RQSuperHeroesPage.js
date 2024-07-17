4aa36055adbc135d9dbd406191caa05351fd09ef
Priyansh Sultania <psultania1000@gmail.com>
2024-07-17 23:11:30 +0530

    Adding onsuccess and onerror on useQuery for data fetching

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -8,19 +8,32 @@ const fetchSuperHeroes = () => {
 };
 
 const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const onSuccess = () => {
+    console.log("Perform side effects after data fetching ");
+  };
+
+  const onError = () => {
+    console.log("Perform side effects on error while data fetching ");
+  };
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
     {
-      cacheTime: 5000, // To cache the data for 5 seconds, default is 5 min
-      staleTime: 10000, // Query will remain fresh for 10 sec instead of getting stale flag, when it is stale it will refetch new data, default is zero
-      refetchOnMount: true, // Default is  true, the query will refetch the data if it is stale
-      refetchOnWindowFocus: true, // Anytime the tab regains focus after losing it. It will refetch the data from server.
+      //   cacheTime: 5000, // To cache the data for 5 seconds, default is 5 min
+      //   staleTime: 10000, // Query will remain fresh for 10 sec instead of getting stale flag, when it is stale it will refetch new data, default is zero
+      //   refetchOnMount: true, // Default is  true, the query will refetch the data if it is stale
+      //   refetchOnWindowFocus: true, // Anytime the tab regains focus after losing it. It will refetch the data from server.
+      //   refetchInterval: 2000, // Default is false, but here it will refetch the query at every 2 seconds.
+      //   refetchIntervalInBackground: true, //refetch data in background
+      //   enabled: false,
+      onSuccess: onSuccess,
+      onError: onError,
     }
   );
 
   console.log({ isLoading, isFetching });
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>...Loading</h2>;
   }
 
@@ -31,6 +44,7 @@ const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
       })}
